9b2572408cf66289fa8bdce294e34c6af3135f34
Emily <emgo0001@stud.kea.dk>
2026-04-17 13:14:22 +0200

    tilføjer på main

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <p>Test</p>
+    <div>
+      <div className="bg-[#EEECED]">
+        <img src="/img/jacket.png" alt="jacket" />
+      </div>
+      <figcaption className="flex gap-40">
+        <div>Jacket</div>
+        <div>$999</div>
+      </figcaption>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <section>
+      <div className="flex gap-15">
+        <h1>HOME</h1>
+        <h1>PRODUCTS</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Navbar;
