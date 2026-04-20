8380292b51e480cb0080b580f8a3293e61e5b059
Louise <lodo0001@stud.kea.dk>
2026-04-20 14:58:25 +0200

    tilføjer den nye forside

--- src/app/page.js
+++ src/app/page.js
@@ -1,45 +1,58 @@
 import { LiaLongArrowAltRightSolid } from "react-icons/lia";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <div className="flex gap-6 items-start ">
-        <div className="transition-transform duration-300 hover:scale-110 origin-top">
-          <img src="./img/jacket.webp" alt="leather jacket"></img>
-          <div className="flex justify-between">
-            <p>JACKET</p>
-            <p>$999</p>
+    <div className="font-bold">
+      <div className="flex gap-6 items-start mt-15">
+        <div className="">
+          <img src="./img/Ribeye.webp" alt="ribeye" className="w-130 h-auto" />
+          <div className="flex justify-between p-1">
+            <p>RIBEYE STEAK</p>
+            <p>$888</p>
           </div>
         </div>
-        <div className="transition-transform duration-300 hover:scale-110 origin-top">
+        <div className="">
           <img src="./img/bag.webp" alt="leather bag"></img>
-          <div className="flex justify-between">
+          <div className="flex justify-between p-1">
             <p>LEATHER BAG</p>
-            <p>$15267</p>
+            <p>$1.267</p>
           </div>
         </div>
-        <div className="transition-transform duration-300 hover:scale-110 origin-top">
-          <img src="./img/glasses.webp" alt="leather glasses"></img>
-          <div className="flex justify-between">
-            <p>GLASSES </p>
-            <p>$5267</p>
+        <div className="">
+          <img
+            src="./img/Anamy_bike.webp"
+            alt="motorbike"
+            className="w-130 h-auto"
+          />
+          <div className="flex justify-between p-1">
+            <p>ANAMY BIKE </p>
+            <p>$52.679</p>
           </div>
         </div>
-        <div className="transition-transform duration-300 hover:scale-110 origin-top">
-          <img src="./img/longsleeve.webp" alt="leather longsleeve"></img>
-          <div className="flex justify-between">
+        <div className="">
+          <img
+            src="./img/longsleeve.webp"
+            alt="leather longsleeve"
+            className="w-130 h-auto"
+          />
+          <div className="flex justify-between p-1">
             <p>LONGSLEEVE </p>
-            <p>$888 </p>
+            <p>$27</p>
           </div>
         </div>
       </div>
-      <div className="flex justify-end items-center gap-2 ">
-        <button className="underline text-3xl cursor-pointer transition-transform duration-300 hover:scale-110 origin-right font-(family-name:<mono_bold>)">
-          VIEW PRODUCTS
-        </button>
-        <LiaLongArrowAltRightSolid
-          size={80}
-          className="cursor-pointer transition-transform duration-300 hover:scale-110 origin-right"
-        />
+      <div className="flex justify-end">
+        <Link href="/products" className="flex items-center gap-3 group">
+          <span className="underline decoration-2 underline-offset-4 text-3xl transition-transform duration-300 group-hover:scale-110 ">
+            VIEW PRODUCTS
+          </span>
+
+          <LiaLongArrowAltRightSolid
+            size={80}
+            className="transition-transform duration-300 group-hover:scale-110"
+          />
+        </Link>
       </div>
     </div>
   );
