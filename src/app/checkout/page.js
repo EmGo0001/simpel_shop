77d12c40ca888cac3afb9f6ffaae39d60e473605
Emily <emgo0001@stud.kea.dk>
2026-04-21 11:13:25 +0200

    tilføjer product + cart

---
+++ src/app/checkout/page.js
@@ -0,0 +1,9 @@
+export default function CheckoutPage() {
+  return (
+    <div>
+      <h1 className="text-xl font-extrabold mt-7 text-center">
+        YOUR SHOPPING BAG
+      </h1>
+    </div>
+  );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,37 @@
+"use client";
+
 import Link from "next/link";
+import { PiBasketLight } from "react-icons/pi";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-  return (
-    <div className="flex gap-10">
-      <Link href="/">
-        <h2 className="text-2xl font-extrabold">HOME</h2>
-      </Link>
+  const pathname = usePathname();
 
-      <Link href="/products">
-        <h2 className="text-2xl font-extrabold">PRODUCTS</h2>
-      </Link>
+  return (
+    <div className="flex items-center w-full justify-between">
+      <div className="flex gap-10">
+        <Link href="/">
+          <h2 className="text-2xl font-extrabold">HOME</h2>
+        </Link>
+
+        <Link href="/products">
+          <h2 className="text-2xl font-extrabold">PRODUCTS</h2>
+        </Link>
+      </div>
+
+      {pathname !== "/" && (
+        <div className="flex items-center gap-2">
+          <Link href="/checkout" className="flex items-center gap-2">
+            <PiBasketLight size={24} />
+            <span className="font-bold">0</span>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Navbar;
+
+// Skjuler cart-ikonet på forsiden ved at tjekke pathname med usePathname()
+// Hvis vi er på "/", bliver cart ikke renderet
